4c2f600c04fd189990ad34d879e7671f85fd4a0c
Eshwarsai-07 <eashwarsaiga@gmail.com>
2025-04-09 19:08:25 +0530

    "This is first react app "

---
+++ src/components/others/Header.jsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { setLocalStorage } from "../../utils/localStorage";
+// const { setUser } = useAuthContext();
+
+const Header = (props) => {
+  // console.log(data)
+  // console.log(props)
+  // const [username, setUsername] = useState("");
+  // console.log(username)
+
+  // if(!data){
+  //   setUsername('Admin')
+  // }else{
+  //   setUsername(data.firstName)
+  // }
+
+  const logOutUser = () => {
+    localStorage.setItem("loggedInUser", "");
+    props.changeUser("");
+    // window.location.reload()
+  };
+
+  return (
+    <div className="flex items-end justify-between">
+      <h1 className="text-2xl font-medium">
+        Hello <br /> <span className="text-3xl font-semibold">usename👋</span>
+      </h1>
+      <button
+        onClick={logOutUser}
+        className="bg-red-600 text-base font-medium text-white px-5 py-2 rounded-sm"
+      >
+        Log Out
+      </button>
+    </div>
+  );
+};
+
+export default Header;
